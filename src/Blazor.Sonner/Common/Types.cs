b2c84703dbae150ed8b213253633994c0c6dd24d
desmondinho <daniil.norinn@gmail.com>
2025-08-27 22:06:36 +0300

    feat(toaster): apply base offset styles

--- src/Blazor.Sonner/Common/Types.cs
+++ src/Blazor.Sonner/Common/Types.cs
@@ -1,5 +1,10 @@
 ﻿namespace Blazor.Sonner.Common;
 
+public sealed record ToastOptions
+{
+	public string? Description { get; set; }
+}
+
 public sealed record ToastModel
 {
 	public Guid Id { get; init; }
@@ -7,9 +12,20 @@ public sealed record ToastModel
 	public string? Description { get; set; }
 }
 
-public sealed record ToastOptions
+public readonly struct Offset( float top, float left, float bottom, float right )
 {
-	public string? Description { get; set; }
+	public readonly float Top = top;
+	public readonly float Left = left;
+	public readonly float Bottom = bottom;
+	public readonly float Right = right;
+
+	public Offset( float xy ) : this( xy, xy, xy, xy ) 
+	{
+	}
+
+	public Offset( float x, float y ) : this( y, x, y, x )
+	{
+	}
 }
 
 public enum Position
---
+++ src/Blazor.Sonner/Common/Utils.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace Blazor.Sonner.Common;
+
+internal static class Utils
+{
+	public static string GetOffset( Offset? offset, Offset? mobileOffset )
+	{
+		var styles = new StringBuilder();
+		var offsets = new Offset?[] { offset, mobileOffset };
+
+		for( var index = 0; index < offsets.Length; index++ )
+		{
+			var current = offsets[index];
+			var isMobile = index == 1;
+			var prefix = isMobile ? "--mobile-offset" : "--offset";
+			var defaultValue = isMobile ? Constants.MobileViewportOffset : Constants.ViewportOffset;
+
+			if( current is null )
+			{
+				AssignAll( prefix, new Offset( defaultValue ) );
+			}
+			else
+			{
+				AssignAll( prefix, current.Value );
+			}
+		}
+
+		return styles.ToString();
+
+		void AssignAll( string prefix, Offset offset )
+		{
+			var offsetToSide = new Dictionary<string, float>
+			{
+				["top"] = offset.Top,
+				["left"] = offset.Left,
+				["bottom"] = offset.Bottom,
+				["right"] = offset.Right
+			};
+
+			foreach( var (key, value) in offsetToSide )
+			{
+				styles.Append( $"{prefix}-{key}: {value}px; " );
+			}
+		}
+	}
+}
