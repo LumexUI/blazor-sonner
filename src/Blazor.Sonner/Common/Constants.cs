4edc71666ed26f7d879a86f4190b6617e74dc068
desmondinho <daniil.norinn@gmail.com>
2025-08-27 19:19:50 +0300

    chore(*): add constants

---
+++ src/Blazor.Sonner/Common/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace Blazor.Sonner.Common;
+
+internal class Constants
+{
+	public const int VisibleToastsAmount = 3;
+
+	public const int ViewportOffset = 24;
+	public const int MobileViewportOffset = 16;
+
+	public const int ToastWidth = 356;
+	public const int GapBetweenToasts = 14;
+
+	public const int ToastLifetime = 4000; // in ms
+	public const int TimeBeforeUnmount = 200; // in ms
+
+	public const int SwipeThreshold = 45;
+}
